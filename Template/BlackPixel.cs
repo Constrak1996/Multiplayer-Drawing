30a06e07e43ed230809ea6c1c54467f818fdb1ff
Alex Frier Stewart <alex7175@student.eadania.dk>
2019-09-05 11:58:30 +0200

    Monogame

---
+++ Template/BlackPixel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class BlackPixel : GameObject
+    {
+        MouseState mouse;
+        
+
+        public BlackPixel(int posX, int posY, string sprite) : base(posX, posY, sprite)
+        {
+            
+        }
+
+        public override void Update()
+        {
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                
+            }
+
+            base.Update();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+    }
+}
---
+++ Template/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    public class GameObject
+    {
+
+        public GameObject(int posX, int posY, string sprite)
+        {
+            
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+    
+    
+}
